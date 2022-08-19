3de1ab046493962898450230f22f3b2b357cd155
kimyubi <ieieie0419@gmail.com>
2022-08-19 20:30:37 +0900

    백트래킹

---
+++ 백트래킹/15652 N과 M(4).py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+s = []
+
+def dfs():
+    if len(s) == m:
+        print(' '.join(map(str,s)))
+        return 
+        
+    for i in range(1, n + 1):
+        if len(s) == 0 or s[-1] <= i:
+            s.append(i)
+            dfs()
+            s.pop()
+           
+dfs()         
