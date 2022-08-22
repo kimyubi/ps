dc7fd85f310d38b5b7b152b3adee908f6890ed8e
kimyubi <ieieie0419@gmail.com>
2022-08-22 21:08:43 +0900

    just practice

---
+++ 백트래킹/15664 N과 M(11).py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+numbers = list(map(int, input().rstrip().split()))
+numbers.sort()
+
+s = []
+is_used = [False for _ in range(n)]
+
+def dfs():
+    pre = 0
+    
+    if len(s) == m:
+        print(' '.join(map(str,s)))
+        return 
+  
+    for i, x in enumerate(numbers):   
+        if pre != x:
+            s.append(x)
+            pre = x
+            
+            dfs()
+            s.pop()
+        
+            
+           
+dfs() 
+
+        
