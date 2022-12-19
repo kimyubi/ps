0c077178a1bad13e641e18bbf98c3661334346fb
kimyubi <ieieie0419@gmail.com>
2022-12-19 10:39:39 +0900

    LCS

---
+++ DP/15468.py
@@ -0,0 +1,26 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+T, P = [], []
+dp = [0] * (N + 1)
+
+for _ in range(N):
+    t, p = map(int, input().split())
+    
+    T.append(t)
+    P.append(p)
+    
+
+for i in range(N):
+    if i + T[i] <= N:
+        dp[i + T[i]] = max(dp[i + T[i]], dp[i] + P[i])
+    
+    dp[i+1] = max(dp[i+1], dp[i])
+    
+print(max(dp))
+    
+        
+        
+    
