04e8f6392c41a7d9a8437909f8682f2972ad9279
kimyubi <ieieie0419@gmail.com>
2023-01-11 16:58:06 +0900

    가장 큰 정사각형

--- DP/1915 가장 큰 정사각형.py
+++ DP/1915 가장 큰 정사각형.py
@@ -3,21 +3,19 @@ import sys
 input = sys.stdin.readline
 n, m = map(int, input().split())
 
+graph = []
+
 # n x m의 0, 1로 된 배열에서 1로 된 가장 큰 정사각형의 크기를 구해라
-graph = [list(map(int, list(input().rstrip()))) for _ in range(n)]
+for _ in range(n):
+    graph.append(list(map(int, input().rstrip())))
 
-def solution():
-    ans = 0
-    for i in range(n):
-        for j in range(m):
-            if i == 0 or j == 0:
-                continue
-            if graph[i][j] != 0:
-                graph[i][j] = min(graph[i-1][j], graph[i][j-1], graph[i-1][j-1]) + 1
+ans = 0
+for i in range(0, n):
+    for j in range(0, m):
+        if i > 0 and j > 0 and graph[i][j] != 0:
+            graph[i][j] += min(graph[i-1][j], graph[i][j-1], graph[i-1][j-1]) 
                 
-            ans = max(graph[i][j], ans)
+        ans = max(graph[i][j], ans)
                 
-    return ans * ans
+print(ans ** ans)
 
-
-print(solution())
