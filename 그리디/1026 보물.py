f15161710c7c1fd86f3785efc773f56128f18c17
kimyubi <ieieie0419@gmail.com>
2023-01-18 12:05:01 +0900

    보물 (그리디)

---
+++ 그리디/1026 보물.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+a.sort(reverse=True)
+b.sort()
+
+ans = 0 
+for x, y in zip(a,b):
+    ans += x * y
+
+print(ans)
