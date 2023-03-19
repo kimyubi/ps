cc2ac8defc1515d3f6808d47434087dfabe48cc6
kimyubi <ieieie0419@gmail.com>
2023-03-19 10:41:57 +0900

    폰켓못 수정

--- 코테/프로그래머스 폰켓못.py
+++ 코테/프로그래머스 폰켓못.py
@@ -1,10 +1,2 @@
-from collections import Counter
 def solution(nums):
-    n = len(nums)
-    type_cnt = len(Counter(nums).keys())
-    
-    if n // 2 <= type_cnt:
-        return n // 2
-    else:
-        return type_cnt
-    
+    return min(len(nums)//2, len(set(nums)))
