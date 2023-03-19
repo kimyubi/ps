5a271f4fd636a5c0e66986704c80776bfa9784f4
kimyubi <ieieie0419@gmail.com>
2023-03-19 11:37:30 +0900

    수정

--- 코테/베스트 앨범.py
+++ 코테/베스트 앨범.py
@@ -1,25 +1,22 @@
 from collections import defaultdict
-
 def solution(genres, plays):
     result = defaultdict(list)
     dic = defaultdict(int)
     
-    i = 0
-    for genre, play in zip(genres, plays):
-        # 속한 노래가 많이 재생된 장르 찾기
+    for i, (genre, play) in enumerate(zip(genres, plays)):
         dic[genre] += play
-        
-        # 장르, 재생 횟수, 고유 번호 기록
         result[genre].append(list((play, i)))
-        i += 1 
     
+    # 속한 노래가 많이 재생된 장르를 먼저 수록한다.
     dic = sorted(dic, key=lambda x: -dic[x])
  
     answer = []
     
     for x in dic:
-        # 1. 장르 내에서 많이 재생된 노래 먼저, 고유 번호가 낮은 노래 먼저
+        # 장르 내에서 많이 재생된 노래 먼저 수록
+        # 재생 횟수가 같으면 고유 번호가 낮은 노래 먼저 수록
         result[x].sort(key=lambda x: (-x[0], x[1]))
+        
         cnt = 1
         for y in result[x]:
             # 장르별로 가장 많이 재생된 노래 두개만 수록
