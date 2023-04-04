7dd578a9e3da2bad4118fab5fea8db5f16314f3d
kimyubi <ieieie0419@gmail.com>
2023-04-04 11:34:02 +0900

    메뉴 리뉴얼 정답

--- 코테/메뉴 리뉴얼.py
+++ 코테/메뉴 리뉴얼.py
@@ -1,38 +1,31 @@
 from itertools import combinations
-from collections import defaultdict
+from collections import Counter, defaultdict
 
 def solution(orders, course):
-    tmp = set()
-    result = []
-    for order in orders:
-        tmp.update(list(order))
-    answer = defaultdict(list)
+    tmp = []
     
+    # 가능한 모든 조합을 찾는다.
     for c in course:
-        for com in combinations(tmp, c):
-            cnt = 0
-            for order in orders:
-                is_contain = True
-                for x in com:
-                    if x not in order:
-                        is_contain = False
-                        break
-                
-                if is_contain:
-                    cnt += 1
-                    
-            if cnt >= 2:
-                answer[c].append([com, cnt])
-        
-        
+        for order in orders:
+            if c <= len(order):
+                for x in combinations(order, c):
+                    tmp.append(''.join(sorted(x)))
+    
+    # 최소 2명 이상의 손님에게서 주문된 구성만 코스요리 후보에 넣는다.
+    counter = Counter(tmp)
+    dic = defaultdict(list)
+    for x in counter:
+        if counter[x] >= 2:
+            dic[len(x)].append([x, counter[x]])
+
+    # 요리 n개로 구성된 코스 요리 중 가장 많은 손님에게 주문된 구성만 고른다.
     result = []
-    for x in answer:
-        for i in answer[x]:
-            if i[-1] == max([t[-1] for t in answer[x]]):
-                result.append(''.join(sorted(i[0])))
-                
-    return sorted(result)
+    for x in dic:
+        for item in dic[x]:
+            if item[-1] == max([i[-1] for i in dic[x]]):
+                result.append(item[0])
+    
+    result.sort()
+    return result
 
             
-                    
-        
