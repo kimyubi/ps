8b42ad4a7c38f4a881661160ab9e2af5c3066848
kimyubi <ieieie0419@gmail.com>
2023-05-26 17:30:08 +0900

    보석 쇼핑 다시 풀기

--- 코테/보석 쇼핑.py
+++ 코테/보석 쇼핑.py
@@ -1,26 +1,35 @@
 def solution(gems):
-    N = len(gems)
-    answer = [0, N-1]
-    kind = len(set(gems))  # 보석종류
-    dic = {gems[0]:1,}  # 종류 체크할 딕셔너리
-    s,e = 0,0  # 투포인터
+    n = len(gems)
     
-    while s<N and e<N:
-        if len(dic) < kind:  # 종류 부족하면 end point 증가 & dic 개수 증가
-            e += 1
-            if e == N:
+    # 보석 종류의 수 kind
+    kind = len(set(gems))
+    answer = [0, n-1]
+    start, end = 0, 0
+    
+    # 보유한 보석의 개수를 나타내는 딕셔너리
+    count_dic = {gems[0] : 1, }
+    
+    while start < n and end < n:
+        # 모든 종류의 보석을 포함하고 있지 않은 경우,
+        if len(count_dic) < kind:
+            end += 1
+            if end == n:
                 break
-            dic[gems[e]] = dic.get(gems[e], 0) + 1
-            
-        else:  # 종류 같으면 ans 비교 후 답 갱신하고, start point 증가 & dic 개수 다운
-            if (e-s+1) < (answer[1]-answer[0]+1):
-                answer = [s,e]
-            if dic[gems[s]] == 1:
-                del dic[gems[s]]
+            count_dic[gems[end]] = count_dic.get(gems[end], 0) + 1
+        
+        # 모든 종류의 보석을 포함한 경우, start를 1 증가시킨다.
+        else:
+            # 현재 구간의 길이가 이전에 저장된 구간의 길이보다 짧은 경우
+            if end - start < answer[1] - answer[0]:
+                answer = [start, end]
+                
+            if count_dic[gems[start]] == 1:
+                del count_dic[gems[start]]
             else:
-                dic[gems[s]] -= 1
-            s += 1
-
+                count_dic[gems[start]] -= 1
+    
+            start += 1
     answer[0] += 1
     answer[1] += 1
+        
     return answer
