efcca79d7cfccb1bb56652c114e3accb0b43a9eb
kimyubi <ieieie0419@gmail.com>
2023-06-27 11:43:30 +0900

    순위 검색(시간 복잡도 통과)

--- 코테/순위 검색.py
+++ 코테/순위 검색.py
@@ -1,27 +1,40 @@
-def solution(info, query):
-    answer = []
-    for q in query:
-        q = q.replace('and', ' ').split()
-        lang, group, career, food, score = map(str, q)
-        score = int(score)
-        cnt = 0
-        
-        for i in info:
-            ilang, igroup, icareer, ifood, iscore = map(str, i.split())
-            iscore = int(iscore)
-            if lang != '-' and ilang != lang:
-                continue
-            elif group != '-' and igroup != group:
-                continue
-            elif career != '-' and icareer != career:
-                continue
-            elif food != '-' and ifood != food:
-                continue
-            elif iscore < score:
-                continue
+from itertools import combinations
+from bisect import bisect_left
+from collections import defaultdict
+
+def make_case(info, infoDict):
+    tempArr = info.split(" ")
+    score = int(tempArr[-1])
+    
+    for idx in range(5):
+        for c in combinations(tempArr[:-1], idx):
+            key = "".join(c)
+            infoDict[key].append(score)
             
-            cnt += 1
-        answer.append(cnt)
+
+# 이진탐색으로 queryScore 보다 큰 점수들의 개수 반환
+def find_answer(key, queryScore, infoDict):
+    if key in infoDict:
+        return len(infoDict[key]) - bisect_left(infoDict[key], queryScore)
+    return 0
+
+def solution(informations, query):
+    infoDict = defaultdict(list)
+    
+    for info in informations:
+        make_case(info, infoDict)
+    
+    for key in infoDict.keys():
+        infoDict[key].sort()
+        
+    answer = []
+    for x in query:
+        tempArr = list(y for y in x.replace("and", " ").replace("-", "").split(" "))
+        queryScore = int(tempArr[-1])
+        
+        if len(tempArr) == 1:
+            answer.append(find_answer("", queryScore, infoDict))
+        else:
+            answer.append(find_answer("".join(tempArr[:-1]), queryScore, infoDict))
             
     return answer
-    
