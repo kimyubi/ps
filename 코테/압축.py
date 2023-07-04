5a9e43dab9cf183ed558bb02981b09dcbed2b80e
kimyubi <ieieie0419@gmail.com>
2023-07-04 15:06:39 +0900

    압축 풀이 완료

--- 코테/압축.py
+++ 코테/압축.py
@@ -1,40 +1,49 @@
+from collections import deque
 def solution(msg):
     answer = []
     dic = dict()
+    
+    # 길이가 1인 모든 단어를 포함하도록 사전을 초기화 한다.
     last = 26
     for i in range(last):
         dic[chr(65 + i)] = i + 1
     
-    point = -1
-    for idx, w in enumerate(msg):
-        if idx <= point:
-            continue
+    msg = deque(msg)
+    while msg:
+        w = msg.popleft()
         
-        next = idx + 1
-        c = msg[idx:next + 1] # KA
-        result = ''
-        tmp = ''
-        if c in dic:
-            while True:
-                if c in dic:
-                    tmp = c
-                    next += 1
-                    c = msg[idx:next + 1]
-                    result = c
-                    break
-                c = msg[idx:next + 1]
-                next += 1
-        else:
+        # 사전에서 현재 입력과 일치하는 가장 긴 문자열 w를 찾는다.
+        tmp = w
+        replace_w = w
+        
+        for idx, s in enumerate(msg):
+            tmp += s
+            if tmp in dic:
+                i = idx
+                replace_w = tmp
+        
+        # w와 replace_w가 같은 경우, 현재 입력과 일치하는 가장 긴 문자열은 w이다.
+        if w == replace_w:
+            # w의 색인 번호 출력
             answer.append(dic[w])
             last += 1
-            dic[c] = last
-            continue
             
-        answer.append(dic[tmp])
-        last += 1
-        dic[result] = last
-        point = next - 1
+            # 입력에서 처리되지 않은 다음 글자가 남아있다면(msg[0]),w+msg[0]에 해당하는 단어를 사전에 등록한다.
+            if msg:
+                dic[w + msg[0]] = last
             
+        else:
+            # w와 replace_w가 다른 경우, 현재 입력과 일치하는 가장 긴 문자열은 replace_w이다.
+            # 입력에서 replace_w를 제거한다.
+            for _ in range(len(replace_w) - 1):
+                w += msg.popleft()
             
+            # replace_w의 색인 번호를 출력
+            answer.append(dic[w])
+            
+            #  현재 입력과 일치하는 가장 긴 문자열 replace_w보다 1만큼 긴 문자열을 사전에 추가한다.
+            last += 1
+            if msg:
+                dic[w + msg[0]] = last
+
     return answer
-        
