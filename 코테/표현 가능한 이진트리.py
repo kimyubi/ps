2f6cf85376292aae3cc4b64fdcb0ab4299de0377
kimyubi <ieieie0419@gmail.com>
2023-07-26 21:21:08 +0900

    표현 가능한 이진트리

--- 코테/표현 가능한 이진트리.py
+++ 코테/표현 가능한 이진트리.py
@@ -1,48 +1,26 @@
-# 살펴본 노드가 더미 노드라면, 문자열 뒤에 0을 추가한다.
-# 살펴본 노드가 더미 노드가 아니라면, 문자열 뒤에 1을 추가한다.
-def find_root(tree):     
-    root_index = len(tree) // 2 -1 if len(tree) % 2 == 0 else len(tree) // 2 
-    left, right = tree[0:root_index], tree[root_index + 1:]
+def find_root(tree):
+    size = len(tree)
+    if size == 1 or 1 not in tree or 0 not in tree:
+        return 1
     
-    # 부모 노드가 0이면 자식 노드가 1이어서는 안된다. (처리 하기)
+    root_index = size // 2
     if not tree[root_index]:
-        if 1 in left or 1 in right:
-            return 0
-   
-    if len(tree[0:root_index]) >= 3:
-        find_root(tree[0:root_index])
-    if len(tree[root_index + 1:]) >= 3:
-        find_root(tree[root_index + 1:])
-    
-    return 1
+        return 0
+
+    left, right = tree[:root_index], tree[root_index + 1:]
+    return find_root(left) and find_root(right)
 
 def make_binary_tree(number):
-    # 10진수를 2진수로 변환한다.
     binary_num = list(map(int, bin(number)[2:]))
     standard = 1
-    print(binary_num)
     while 2 ** standard -1 < len(binary_num):
         standard += 1
     binary_num = [0] * (2 ** standard -1 - len(binary_num)) + binary_num
-
-    root_index = len(binary_num) // 2 -1 if len(binary_num) % 2 == 0 else len(binary_num) // 2 
-    print(binary_num[0:root_index], binary_num[root_index], binary_num[root_index + 1:])
-    # root가 0이면 이진트리로 표현할 수 없다.
-    if not binary_num[root_index]:
-        return 0
-    if not find_root(binary_num[0:root_index]):
-        return 0
-    if not find_root(binary_num[root_index + 1:]):
-        return 0
-    
-    return 1
+    return find_root(binary_num)
+ 
 
 def solution(numbers):
     answer = []
-    
     for number in numbers:
-        if number == 1:
-            answer.append(1)
-        else:
-            answer.append(make_binary_tree(number))
+        answer.append(make_binary_tree(number))
     return answer
