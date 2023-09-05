040ba9ce48b85fbc82da00f3710c4f04e5a39461
kimyubi <ieieie0419@gmail.com>
2023-09-05 17:27:41 +0900

    LCS2

--- DP/DP 복습/9252 LCS2.py
+++ DP/DP 복습/9252 LCS2.py
@@ -1,25 +1,23 @@
 import sys
 input = sys.stdin.readline
 
-s1 = input()
-s2 =  input()
+s1 = ' ' + input().rstrip()
+s2 =  ' ' + input().rstrip()
 
 s1_len = len(s1)
 s2_len = len(s2)
 
-dp = [[''] * (s2_len + 1) for _ in range(s1_len + 1)]
+dp = [[''] * s2_len for _ in range(s1_len)]
 
-s1, s2 = ' ' + s1, ' ' + s2
-for i in range(1, s1_len + 1):
-    for j in range(1, s2_len + 1):
+for i in range(1, s1_len):
+    for j in range(1, s2_len):
         if s1[i] == s2[j]:
             dp[i][j] = dp[i-1][j-1] + s1[i]
-        
-        
         else:
             if len(dp[i-1][j]) > len(dp[i][j-1]):
                 dp[i][j] = dp[i-1][j]
             else:
                 dp[i][j] = dp[i][j-1]
-            
-print(dp[-1][-1])
+
+result = dp[-1][-1]
+print(len(result), result, sep = "\n")
