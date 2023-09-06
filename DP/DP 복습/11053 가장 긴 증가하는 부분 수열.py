f6ba41652747737ec4d6dcc02aa4b13ce088985c
kimyubi <ieieie0419@gmail.com>
2023-09-06 17:11:32 +0900

    가장 긴 증가하는 부분 수열

--- DP/DP 복습/11053 가장 긴 증가하는 부분 수열.py
+++ DP/DP 복습/11053 가장 긴 증가하는 부분 수열.py
@@ -7,11 +7,8 @@ sequence = list(map(int, input().split()))
 dp = [1] * n
 
 for i in range(n):
-    max_value = 0
     for j in range(i):
         if sequence[j] < sequence[i]:
-            max_value = max(max_value, dp[j])
-            
-    dp[i] = max_value + 1
+            dp[i] = max(dp[i], dp[j] + 1)       
     
 print(max(dp))
