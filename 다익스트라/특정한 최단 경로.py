2a8fe64228f72b127dbfcfd4748d77ac35e16325
kimyubi <ieieie0419@gmail.com>
2023-10-10 15:46:34 +0900

    특정한 최단 경로

--- 다익스트라/특정한 최단 경로.py
+++ 다익스트라/특정한 최단 경로.py
@@ -38,12 +38,12 @@ def dijkstra(start):
                 
     return distance
 
+original_distance = dijkstra(1)
+v1_distance = dijkstra(v1)
+v2_distance = dijkstra(v2)
 
-result1_v1, resultv1_v2, resultv2_n = dijkstra(1)[v1], dijkstra(v1)[v2], dijkstra(v2)[n]
-result1_v2, resultv1_n = dijkstra(1)[v2], dijkstra(v1)[n]
+v1_path = original_distance[v1] + v1_distance[v2] + v2_distance[n]
+v2_path = original_distance[v2] + v2_distance[v1] + v1_distance[n]
 
-if INF in(result1_v1, resultv1_v2, resultv2_n) and INF in(result1_v2, resultv1_v2, resultv1_n):
-    print(-1)
-    
-else:
-    print(min(result1_v1 +  resultv1_v2 +  resultv2_n, result1_v2 +  resultv1_v2 +  resultv1_n))
+result = min(v1_path, v2_path)
+print(result if result < INF else -1)
