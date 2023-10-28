5bbfdec68f43190d145c55114fa95d39c0f40a94
kimyubi <ieieie0419@gmail.com>
2023-10-28 22:07:25 +0900

    2048(Easy)

--- 삼성 기출 문제집/2048 (Easy).py
+++ 삼성 기출 문제집/2048 (Easy).py
@@ -10,19 +10,98 @@ board = [list(map(int, input().split())) for _ in range(n)]
 # 같은 값을 갖는 두 블록이 충돌하면 두 블록은 하나로 합쳐진다.
 # 한번의 이동에서 이미 합쳐진 블록은 또 다른 블록과 합쳐질 수 없다.
 # 최대 5번 이동시켜서 얻을 수 있는 가장 큰 블록을 출력한다.
-dx = [-1, 1, 0, 0] 
-dy = [0, 0, -1, 1]
 
 def move(board, direction):
-    return
+    # 왼쪽으로 이동
+    if direction == 0:
+        for i in range(n):
+            pointer = 0
+            for j in range(1, n):
+                if board[i][j]:
+                    tmp = board[i][j]
+                    board[i][j] = 0
+                    
+                    if not board[i][pointer]:
+                        board[i][pointer] = tmp
+                    
+                    elif board[i][pointer] == tmp:
+                        board[i][pointer] *= 2
+                        pointer += 1
+                    else:
+                        pointer += 1
+                        board[i][pointer] = tmp
+                        
+    # 오른쪽으로 이동
+    if direction == 1:
+        for i in range(n):
+            pointer = n - 1
+            for j in range(n-2, -1, -1):
+                if board[i][j]:
+                    tmp = board[i][j]
+                    board[i][j] = 0
+                    
+                    if not board[i][pointer]:
+                        board[i][pointer] = tmp
+                        
+                    elif board[i][pointer] == tmp:
+                        board[i][pointer] *= 2
+                        pointer -= 1
+                        
+                    else:
+                        pointer -= 1
+                        board[i][pointer] = tmp
+                        
+    
+    # 위쪽으로 이동
+    if direction == 2:
+        for j in range(n):
+            pointer = 0
+            for i in range(1, n):
+                if board[i][j]:
+                    tmp = board[i][j]
+                    board[i][j] = 0
+                    
+                    if not board[pointer][j]:
+                        board[pointer][j] = tmp
+                            
+                    elif board[pointer][j] == tmp:
+                        board[pointer][j] *= 2
+                        pointer += 1
+                            
+                    else:
+                        pointer += 1
+                        board[pointer][j] = tmp
+                        
+                
+    # 아래쪽으로 이동
+    if direction == 3:
+        for j in range(n):
+            pointer = n-1
+            for i in range(n-2, -1, -1):
+                if board[i][j]:
+                    tmp = board[i][j]
+                    board[i][j] = 0
+                    
+                    if not board[pointer][j]:
+                        board[pointer][j] = tmp
+                        
+                    elif board[pointer][j] == tmp:
+                        board[pointer][j] *= 2
+                        pointer -= 1
+                        
+                    else:
+                        pointer -= 1
+                        board[pointer][j] = tmp
+    
+    return board                                 
+                
+                
 
 answer = 0  
 def solution(board, depth):
     global answer
     if depth == 5:
-        for i in range(n):
-            for j in range(n):
-                answer = max(answer, board[i][j])
+        answer = max(answer, max(map(max, board)))
         return
         
     for i in range(4):
