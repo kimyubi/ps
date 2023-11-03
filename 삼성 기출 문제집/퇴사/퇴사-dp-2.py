342778531e8015867fe3fc7a8c0fdd6cb3ae3e4e
kimyubi <ieieie0419@gmail.com>
2023-11-03 22:18:01 +0900

    퇴사 -dp(앞에서부터)

--- 삼성 기출 문제집/퇴사/퇴사-dp-2.py
+++ 삼성 기출 문제집/퇴사/퇴사-dp-2.py
@@ -3,9 +3,21 @@ input = sys.stdin.readline
 
 # 오늘부터 N+1일째 되는 날 퇴사를 하기 위해서, 남은 N일 동안 최대한 많은 상담을 하려고 한다.
 N = int(input())
-T, P = [0], [0]
+T, P = [], []
 
 for _ in range(N):
     t, p = map(int, input().split())
     T.append(t)
     P.append(p)
+    
+dp = [0] * (N + 1)
+
+for i in range(N):
+    if N < i + T[i]:
+        continue
+    
+    dp[i] = max(dp[:i + 1])
+    dp[i + T[i]] = max(dp[i + T[i]], dp[i] + P[i])
+    
+
+print(max(dp))
