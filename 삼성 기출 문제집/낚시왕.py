126198566fd178c9327498cc0469700b4e978567
kimyubi <ieieie0419@gmail.com>
2023-11-17 13:28:34 +0900

    낚시왕

--- 삼성 기출 문제집/낚시왕.py
+++ 삼성 기출 문제집/낚시왕.py
@@ -1,6 +1,5 @@
 # 12:50 ~
 import sys
-from copy import deepcopy
 input = sys.stdin.readline
 
 # 위, 아래, 오른쪽, 왼쪽
@@ -10,7 +9,7 @@ dy = [0, 0, 0, 1, -1]
 # 격자판의 크기 r * c, 상어의 수 m
 r, c, m = map(int, input().split())
 
-board = [[[] for _ in range(c)] for _ in range(r)]
+board = [[list() for _ in range(c)] for _ in range(r)]
 for _ in range(m):
     x, y, s, d, z = map(int, input().split())
     board[x-1][y-1].append([s, d, z])
@@ -20,89 +19,64 @@ for _ in range(m):
 pointer = -1
 answer = 0
 
-for t in board:
-    print(t)
 
-while True:
+while 0 < m:
     # 1. 낚시왕이 오른쪽으로 한 칸 이동한다.
     pointer += 1
     
     if pointer == c:
         break
-
+    
     # 낚시왕이 있는 열에 있는 상어 중에서 땅과 제일 가까운 상어를 잡는다. 
     # 상어를 잡으면 격자판에서 잡은 상어가 사라진다.
     for idx in range(r):
         if board[idx][pointer]:
-            answer += board[idx][pointer][0][-1]
-            board[idx][pointer][0].clear()
+            answer += board[idx][pointer].pop()[-1]
             break
         
-    # 상어가 s칸 이동한다.
-    # 상어가 이동하려고 하는 칸이 격자판의 경계를 넘는 경우에는 방향을 반대로 바꿔서 속력을 유지한채로 이동한다.
-    visited = []
-    for x in range(r):
-        for y in range(c):
-            if board[x][y]:
-                if board[x][y][0]:
-                    sx, sy = x, y
-                    s, d, z = board[x][y][0]
-                    if z in visited:
-                        continue
-                    cnt = 0
-                    while True:
-                        if cnt == s:
-                            ps, pv, pz = board[sx][sy].pop()
-                            board[x][y].append([ps, d, pz])
-                            visited.append(pz)
-                            break
-                        
-                        nx, ny = x + dx[d], y + dy[d]
-                        if 0 <= nx < r and 0 <= ny < c:
-                            cnt += 1
-                            x, y = nx, ny
-                            
-                        # 상어가 이동하려고 하는 칸이 격자판의 경계를 넘는 경우에는 방향을 반대로 바꿔서 속력을 유지한채로 이동한다.
-                        else:
-                            # 위, 아래, 오른쪽, 왼쪽
-                            if d == 1:
-                                d = 2
-                                continue
-                            elif d == 2:
-                                d = 1
-                                continue
-                            elif d == 3:
-                                d = 4
-                                continue
-                            else:
-                                d = 3
-                                continue
-    
-    print(pointer)           
-    for t in board:
-        print(t)
-        
-    print()
-                              
-    
-    # 상어가 이동을 마친 후에 한 칸에 상어가 두 마리 이상 있을 수 있다. 이때는 크기가 가장 큰 상어가 나머지 상어를 모두 잡아먹는다.   
+    sharks = []
     for i in range(r):
         for j in range(c):
-            if board[i][j]:
-                if 2 <= len(board[i][j]):
-                    board[i][j].sort(key=lambda x: x[-1])
-                        
-                    for idx in range(len(board[i][j]) -1):
-                        board[i][j][-1][-1] += board[i][j][idx][-1]
-                        
-                    larges_shark = board[i][j][-1]
+            if board[i][j] and board[i][j][0]:
+                sharks.append([board[i][j][0], (i, j)])
                     
-                    board[i][j].clear()
-                    board[i][j].append(larges_shark)             
-                    
-                    
-    
-                
-print(answer)
+    while sharks:
+        shark, position = sharks.pop()
+        s, d, z = shark
+        x, y = position
+        
+        # 초기 x,y 좌표와 방향
+        fx, fy = x, y
+        fd = d
+        cnt = 0
             
+        while True:
+            if cnt == s:
+                board[fx][fy].remove([s, fd, z])
+                board[x][y].append([s, d, z])
+                break
                 
+            nx, ny = x + dx[d], y + dy[d]
+            if 0 <= nx < r and 0 <= ny < c:
+                cnt += 1
+                x, y = nx, ny
+                    
+            else:
+                if d == 1:
+                    d = 2
+                elif d == 2:
+                    d = 1
+                elif d == 3:
+                    d = 4
+                else:
+                    d = 3
+    
+    # 상어가 이동을 마친 후에 한 칸에 상어가 두 마리 이상 있을 수 있다. 이때는 크기가 가장 큰 상어가 나머지 상어를 모두 잡아먹는다.
+    for i in range(r):
+        for j in range(c):
+            if board[i][j] and 2 <= len(board[i][j]):
+                board[i][j].sort(key=lambda x: x[-1])
+                del board[i][j][:-1]
+               
+                 
+print(answer)
