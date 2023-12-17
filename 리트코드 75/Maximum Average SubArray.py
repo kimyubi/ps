4d974b683a91d05560090e11ce697d5c294048d4
kimyubi <ieieie0419@gmail.com>
2023-12-17 22:20:05 +0900

    Maximum Average SubArray

--- 리트코드 75/Maximum Average SubArray.py
+++ 리트코드 75/Maximum Average SubArray.py
@@ -13,3 +13,18 @@ class Solution:
             prev = tmp - nums[i]
                 
         return max_sum / k
+    
+# 개선
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        currSum = maxSum = sum(nums[:k])
+
+        for i in range(k, len(nums)):
+            currSum += nums[i] - nums[i - k]
+            maxSum = max(maxSum, currSum)
+
+        return maxSum / k
+
+            
+        
+        
